57b2a325c654616335b657d583be78c6fdf8b95d
AionJayT <jay.tseng@aion.network>
2019-05-06 15:43:08 -0400

    update aion api version to v0.1.16 and add getBlockReward test

--- Tests/test/org/aion/harness/tests/integ/JavaApiSmokeTest.java
+++ Tests/test/org/aion/harness/tests/integ/JavaApiSmokeTest.java
@@ -40,65 +40,69 @@ import org.aion.harness.util.SimpleLog;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JavaApiSmokeTest {
     private static final String BUILT_KERNEL = System.getProperty("user.dir") + "/aion";
-    private static final String PREMINED_KEY = "4c3c8a7c0292bc55d97c50b4bdabfd47547757d9e5c194e89f66f25855baacd0";
+    private static final String PREMINED_KEY =
+            "4c3c8a7c0292bc55d97c50b4bdabfd47547757d9e5c194e89f66f25855baacd0";
     private static final long ENERGY_LIMIT = 1_234_567L;
     private static final long ENERGY_PRICE = 10_010_020_345L;
 
-    private static final SimpleLog log = new SimpleLog("org.aion.harness.tests.integ.JavaApiSmokeTest");
+    private static final SimpleLog log =
+            new SimpleLog("org.aion.harness.tests.integ.JavaApiSmokeTest");
 
     private static LocalNode node;
     private static RPC rpc;
     private static NodeListener listener;
     private static PrivateKey preminedPrivateKey;
+    private static IAionAPI api;
 
     @BeforeClass
     public static void setup() throws Exception {
         ProhibitConcurrentHarness.acquireTestLock();
         preminedPrivateKey = PrivateKey.fromBytes(Hex.decodeHex(PREMINED_KEY));
-
-        NodeConfigurations configurations = NodeConfigurations.alwaysUseBuiltKernel(Network.CUSTOM, BUILT_KERNEL, DatabaseOption.PRESERVE_DATABASE);
-
-        node = NodeFactory.getNewLocalNodeInstance(NodeType.JAVA_NODE);
-        node.configure(configurations);
-        Result result = node.initialize();
-        log.log(result);
-        assertTrue(result.isSuccess());
-        Result startResult = node.start();
-        assertTrue("Kernel startup error: " + startResult.getError(),
-            startResult.isSuccess());
-        assertTrue(node.isAlive());
-        rpc = new RPC("127.0.0.1", "8545");
-        listener = NodeListener.listenTo(node);
+        api = AionAPIImpl.inst();
     }
 
-    @AfterClass
-    public static void tearDown() throws Exception {
+    @After
+    public void close() throws IOException, InterruptedException {
         System.out.println("Node stop: " + node.stop());
         node = null;
         rpc = null;
         listener = null;
+        api.destroyApi();
+        Thread.sleep(3000);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+
         destroyLogs();
         ProhibitConcurrentHarness.releaseTestLock();
     }
 
     @Test(timeout = 300_000 /* millis */)
     public void testGetBlockDetailsByRange() throws Exception {
-        IAionAPI api = AionAPIImpl.inst();
-        ApiMsg connectionMsg = api.connect("tcp://localhost:8547");
-        int tries = 0;
 
-        while(connectionMsg.isError() && tries++ <= 10) {
-            log.log("trying again after 3 sec");
-            connectionMsg = api.connect("tcp://localhost:8547");
-        }
-        if(connectionMsg.isError()) {
-            throw new RuntimeException("error: aion_api can't connect to kernel (after retrying 10 times).");
+        setupNodeEnv(Network.CUSTOM);
+
+        if (!api.isConnected()) {
+
+            ApiMsg connectionMsg = api.connect("tcp://localhost:8547");
+            int tries = 0;
+
+            while (connectionMsg.isError() && tries++ <= 10) {
+                log.log("trying again after 3 sec");
+                connectionMsg = api.connect("tcp://localhost:8547");
+            }
+            if (connectionMsg.isError()) {
+                throw new RuntimeException(
+                        "error: aion_api can't connect to kernel (after retrying 10 times).");
+            }
         }
 
         // send a transaction that deploys a simple contract and loads it with some funds
@@ -113,50 +117,155 @@ public class JavaApiSmokeTest {
         long bn = b0;
 
         // wait for two more blocks
-        while(bn < b2) {
+        while (bn < b2) {
             log.log("current block number = " + bn + "; waiting to reach block number " + b2);
             TimeUnit.SECONDS.sleep(10); // expected block time
             bn = rpc.blockNumber().getResult();
         }
-
 
         log.log(String.format("Calling getBlockDetailsByRange(%d, %d)", b0, b2));
         ApiMsg blockDetailsMsg = api.getAdmin().getBlockDetailsByRange(b0, b2);
         assertThat(blockDetailsMsg.isError(), is(false));
 
         List<BlockDetails> blockDetails = blockDetailsMsg.getObject();
-        assertThat("incorrect number of blocks",
-            blockDetails.size(), is(3));
-        assertThat("block details has incorrect block number",
-            blockDetails.get(0).getNumber(), is(b0));
-        assertThat("block details has incorrect block number",
-            blockDetails.get(1).getNumber(), is(b0 + 1));
-        assertThat("block details has incorrect block number",
-            blockDetails.get(2).getNumber(), is(b2));
+        assertThat("incorrect number of blocks", blockDetails.size(), is(3));
+        assertThat(
+                "block details has incorrect block number",
+                blockDetails.get(0).getNumber(),
+                is(b0));
+        assertThat(
+                "block details has incorrect block number",
+                blockDetails.get(1).getNumber(),
+                is(b0 + 1));
+        assertThat(
+                "block details has incorrect block number",
+                blockDetails.get(2).getNumber(),
+                is(b2));
 
         BlockDetails b0Details = blockDetails.get(0);
-        assertThat("block details has incorrect number of transactions",
-            b0Details.getTxDetails().size(), is(1));
-        assertThat("block details' tx details incorrect contract address",
-            b0Details.getTxDetails().get(0).getContract(), is(not(nullValue())));
-        assertThat("block details' tx details incorrect value",
-            b0Details.getTxDetails().get(0).getValue().equals(amount), is(true));
+        assertThat(
+                "block details has incorrect number of transactions",
+                b0Details.getTxDetails().size(),
+                is(1));
+        assertThat(
+                "block details' tx details incorrect contract address",
+                b0Details.getTxDetails().get(0).getContract(),
+                is(not(nullValue())));
+        assertThat(
+                "block details' tx details incorrect value",
+                b0Details.getTxDetails().get(0).getValue().equals(amount),
+                is(true));
+    }
+
+    private void setupNodeEnv(Network network) throws IOException, InterruptedException {
+        NodeConfigurations configurations =
+                NodeConfigurations.alwaysUseBuiltKernel(
+                        network, BUILT_KERNEL, DatabaseOption.PRESERVE_DATABASE);
+
+        node = NodeFactory.getNewLocalNodeInstance(NodeType.JAVA_NODE);
+        node.configure(configurations);
+        Result result = node.initialize();
+        log.log(result);
+        assertTrue(result.isSuccess());
+        Result startResult = node.start();
+        assertTrue("Kernel startup error: " + startResult.getError(), startResult.isSuccess());
+        assertTrue(node.isAlive());
+        rpc = new RPC("127.0.0.1", "8545");
+        listener = NodeListener.listenTo(node);
+    }
+
+    @Test(timeout = 30_000 /* millis */)
+    public void testGetBlockRewards() throws IOException, InterruptedException {
+
+        setupNodeEnv(Network.MAINNET);
+
+        if (!api.isConnected()) {
+            ApiMsg connectionMsg = api.connect("tcp://localhost:8547");
+            int tries = 0;
+
+            while (connectionMsg.isError() && tries++ <= 10) {
+                log.log("trying again after 3 sec");
+                connectionMsg = api.connect("tcp://localhost:8547");
+            }
+            if (connectionMsg.isError()) {
+                throw new RuntimeException(
+                        "error: aion_api can't connect to kernel (after retrying 10 times).");
+            }
+        }
+
+        // Get blockrewards by given block number.
+        log.log("Calling getBlockReward(1)");
+        ApiMsg blockRewardMsg = api.getChain().getBlockReward(1);
+        assertThat(blockRewardMsg.isError(), is(false));
+        BigInteger rewards = blockRewardMsg.getObject();
+        assertThat(
+                "The blockreward of the first block is",
+                rewards.toString(),
+                is("748997531261476163"));
+
+        log.log("Calling getBlockReward(3110400)");
+        blockRewardMsg = api.getChain().getBlockReward(3110400);
+        assertThat(blockRewardMsg.isError(), is(false));
+        rewards = blockRewardMsg.getObject();
+        assertThat(
+                "The blockreward of the block 3110400 is",
+                rewards.toString(),
+                is("1497989283243310185"));
+
+        // 0.4 HF#
+        log.log("Calling getBlockReward(3360000)");
+        blockRewardMsg = api.getChain().getBlockReward(3360000);
+        assertThat(blockRewardMsg.isError(), is(false));
+        rewards = blockRewardMsg.getObject();
+        assertThat(
+                "The blockreward of the block 3360000 is",
+                rewards.toString(),
+                is("1497989283243310185"));
+
+        log.log("Calling getBlockReward(3360001)");
+        blockRewardMsg = api.getChain().getBlockReward(3360001);
+        assertThat(blockRewardMsg.isError(), is(false));
+        rewards = blockRewardMsg.getObject();
+        assertThat(
+                "The blockreward of the block 3360001 is",
+                rewards.toString(),
+                is("1513859186344652359"));
+
+        log.log("Calling getBlockReward(6470400)");
+        blockRewardMsg = api.getChain().getBlockReward(3360000 + 3110400);
+        assertThat(blockRewardMsg.isError(), is(false));
+        rewards = blockRewardMsg.getObject();
+        assertThat(
+                "The blockreward of the block 6470400 is",
+                rewards.toString(),
+                is("1513859186344652359"));
+
+        log.log("Calling getBlockReward(6470401)");
+        blockRewardMsg = api.getChain().getBlockReward(3360000 + 3110400 + 1);
+        assertThat(blockRewardMsg.isError(), is(false));
+        rewards = blockRewardMsg.getObject();
+        assertThat(
+                "The blockreward of the block 6470401 is",
+                rewards.toString(),
+                is("1528997778208098882"));
     }
 
     private BigInteger getNonce() throws InterruptedException {
-        RpcResult<BigInteger> nonceResult = this.rpc.getNonce(this.preminedPrivateKey.getAddress());
+        RpcResult<BigInteger> nonceResult = rpc.getNonce(preminedPrivateKey.getAddress());
         assertRpcSuccess(nonceResult);
         return nonceResult.getResult();
     }
 
-    private TransactionReceipt sendTransaction(RawTransaction transaction) throws InterruptedException {
+    private TransactionReceipt sendTransaction(RawTransaction transaction)
+            throws InterruptedException {
         // we want to ensure that the transaction gets sealed into a block.
         IEvent transactionIsSealed = PrepackagedLogEvents.getTransactionSealedEvent(transaction);
-        FutureResult<LogEventResult> future = this.listener.listenForEvent(transactionIsSealed, 5, TimeUnit.MINUTES);
+        FutureResult<LogEventResult> future =
+                listener.listenForEvent(transactionIsSealed, 5, TimeUnit.MINUTES);
 
         // Send the transaction off.
         log.log("Sending the transaction...");
-        RpcResult<ReceiptHash> sendResult = this.rpc.sendTransaction(transaction);
+        RpcResult<ReceiptHash> sendResult = rpc.sendTransaction(transaction);
         assertRpcSuccess(sendResult);
 
         // Wait on the future to complete and ensure we saw the transaction get sealed.
@@ -167,7 +276,7 @@ public class JavaApiSmokeTest {
 
         ReceiptHash hash = sendResult.getResult();
 
-        RpcResult<TransactionReceipt> receiptResult = this.rpc.getTransactionReceipt(hash);
+        RpcResult<TransactionReceipt> receiptResult = rpc.getTransactionReceipt(hash);
         assertRpcSuccess(receiptResult);
         return receiptResult.getResult();
     }
@@ -176,15 +285,17 @@ public class JavaApiSmokeTest {
         FileUtils.deleteDirectory(new File(System.getProperty("user.dir") + "/logs"));
     }
 
-    private RawTransaction buildTransactionToCreateAndTransferToFvmContract(BigInteger amount) throws DecoderException, InterruptedException {
-        TransactionResult result = RawTransaction.buildAndSignGeneralTransaction(
-            preminedPrivateKey,
-            getNonce(),
-            null,
-            getFvmContractBytes(),
-            ENERGY_LIMIT,
-            ENERGY_PRICE,
-            amount);
+    private RawTransaction buildTransactionToCreateAndTransferToFvmContract(BigInteger amount)
+            throws DecoderException, InterruptedException {
+        TransactionResult result =
+                RawTransaction.buildAndSignGeneralTransaction(
+                        preminedPrivateKey,
+                        getNonce(),
+                        null,
+                        getFvmContractBytes(),
+                        ENERGY_LIMIT,
+                        ENERGY_PRICE,
+                        amount);
 
         assertTrue(result.isSuccess());
         return result.getTransaction();
@@ -193,16 +304,18 @@ public class JavaApiSmokeTest {
     /**
      * Returns the bytes of an FVM contract named 'PayableConstructor'.
      *
-     * See src/org/aion/harness/tests/contracts/fvm/PayableConstructor.sol for the contract itself.
+     * <p>See src/org/aion/harness/tests/contracts/fvm/PayableConstructor.sol for the contract
+     * itself.
      *
-     * We use the bytes directly here, just because we don't have any good utilities in place for
+     * <p>We use the bytes directly here, just because we don't have any good utilities in place for
      * generating these bytes ourselves.
      */
     private byte[] getFvmContractBytes() throws DecoderException {
-        return Hex.decodeHex("60506040525b5b600a565b6088806100186000396000f300605060405260"
-            + "00356c01000000000000000000000000900463ffffffff1680634a6a740714603b578063fc9ad4331"
-            + "46043576035565b60006000fd5b60416056565b005b3415604e5760006000fd5b60546059565b005b"
-            + "5b565b5b5600a165627a7a723058206bd6d88e9834838232f339ec7235f108a21441649a2cf876547"
-            + "229e6c18c098c0029");
+        return Hex.decodeHex(
+                "60506040525b5b600a565b6088806100186000396000f300605060405260"
+                        + "00356c01000000000000000000000000900463ffffffff1680634a6a740714603b578063fc9ad4331"
+                        + "46043576035565b60006000fd5b60416056565b005b3415604e5760006000fd5b60546059565b005b"
+                        + "5b565b5b5600a165627a7a723058206bd6d88e9834838232f339ec7235f108a21441649a2cf876547"
+                        + "229e6c18c098c0029");
     }
 }
